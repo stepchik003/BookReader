e2d1d4c5aa8467a3433ae7495d752fd0edb012c5
stepchik003 <stepan.alex.003@gmail.com>
2025-11-21 12:01:16 +0300

    feat(core-navigation): add app routes

--- core/navigation/src/main/java/com/example/navigation/AppRoute.kt
+++ core/navigation/src/main/java/com/example/navigation/AppRoute.kt
@@ -5,18 +5,36 @@ import kotlinx.serialization.Serializable
 sealed interface AppRoute {
 
     @Serializable
-    data object AuthGraph : AppRoute
-
-    @Serializable
-    data object HomeGraph : AppRoute
-
-    @Serializable
     data object LoadingScreen : AppRoute
-
     @Serializable
-    data object LoginScreen : AppRoute
-
+    data object AuthGraph : AppRoute
     @Serializable
-    data object RegisterScreen : AppRoute
+    data object MainContentGraph : AppRoute
 
+}
+sealed interface AuthRoute : AppRoute {
+    @Serializable
+    data object LoginScreen : AuthRoute
+    @Serializable
+    data object RegisterScreen : AuthRoute
+}
+
+sealed interface MainContentRoute : AppRoute {
+    @Serializable
+    data object BooksScreen : MainContentRoute
+    @Serializable
+    data object UploadScreen : MainContentRoute
+    @Serializable
+    data object ProfileScreen : MainContentRoute
+}
+
+
+sealed interface ReaderRoute : AppRoute {
+    @Serializable
+    data class BookReaderScreen(val bookId: String) : ReaderRoute
+}
+
+sealed interface ProfileRoute : AppRoute {
+    @Serializable
+    data object SettingsScreen : ProfileRoute
 }
